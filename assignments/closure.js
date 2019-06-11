801750d7a1d9796a0e91f19ad5dbd156d9dfcafa
adriancobon <adriancobon@outlook.com>
2019-06-11 12:23:36 -0400

    Finished closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function artist() {
+  let name = "Georgia O'Keefe";
+  function displayArtist() {
+    alert(name);
+  }
+  displayArtist();
+}
+
+artist();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
